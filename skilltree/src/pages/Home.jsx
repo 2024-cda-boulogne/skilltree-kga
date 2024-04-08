e47e9beaadb80c15e535d70e32d72b685debeeda
JunkBezoul <alex.buffet7839@gmail.com>
2024-04-08 11:50:57 +0200

    :art: style: enhance Home.css and Home.jsx layout and design

--- skilltree/src/pages/Home.jsx
+++ skilltree/src/pages/Home.jsx
@@ -4,7 +4,18 @@ import '../Home.css';
 
 function Home() {
   return (
-    <div></div>
+    <div>
+      <div className='main-window window-color'>
+        
+      </div>
+      <div className='hour-window window-color'>
+        Fin de Formation : <br/>
+        Apprenants : 12
+      </div>
+      <div className='area-window window-color'>
+        Boulogne-sur-Mer
+      </div>
+    </div>
   );
 }
 
