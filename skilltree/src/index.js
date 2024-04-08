39c985117ae6c604b5aa84d3ac88440c7a10123b
Karl-Gavois <Gavois.k@hotmail.com>
2024-04-08 16:30:07 +0200

    feat: :sparkles: Add Details page

--- skilltree/src/index.js
+++ skilltree/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+import Details from './pages/Details';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Details />
   </React.StrictMode>
 );
 
