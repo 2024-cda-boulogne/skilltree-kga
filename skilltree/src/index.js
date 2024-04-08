3b7936312db9e295bb75206d03015467bb9f20bd
Ericdsr <eric62.dasilvarocha@laposte.net>
2024-04-08 11:19:33 +0200

    Add accordion components using Flowbite and integrate Tailwind CSS for styling

--- skilltree/src/index.js
+++ skilltree/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+import Test from './pages/test';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Test />
   </React.StrictMode>
 );
 
