7d8aed697c1d585eb0c255a0c19779aaf173a1b8
Ericdsr <eric62.dasilvarocha@laposte.net>
2024-04-10 12:14:22 +0200

    Add Supabase integration for data fetching and update UI components

--- skilltree/src/pages/Home.jsx
+++ skilltree/src/pages/Home.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Home.css';
-
+import supabase from '../services/supabase';
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = await supabase
+        .from('Learners')
+        .select('*');
+        console.log(data);
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      setData(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div>
+      <div className='dflexresize'>
+        {data.map((Learner) => (
+          <div key={Learner.id}>
+                <div className="image-bordure">
+                  <div className='photo'> 
+                    <img src={Learner.url} alt="" className='photomg' />
+                  </div>
+                </div>
+          </div>
+        ))}
+      </div>
       <div>
         <div className='showskill-window'>
           <div className='skill-window'>
