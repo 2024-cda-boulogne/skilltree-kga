fc2cf3ffbf3fef76f779607a8c6990b64820aa83
JunkBezoul <alex.buffet7839@gmail.com>
2024-04-10 12:14:17 +0200

    :art: style: update CSS properties and structure for better UI design

--- skilltree/src/pages/Home.jsx
+++ skilltree/src/pages/Home.jsx
@@ -6,20 +6,25 @@ function Home() {
   return (
     <div>
       <div>
-        <div className='showskill-window'>
-          <div className='skill-window'>
-            <p className='text-black'>Développer une application sécurisée</p>
-          </div>
-          <div className='skill-window'>
-            <p className='text-black'>Concevoir et développer une application sécurisée organisée en couches</p>
-          </div>
-          <div className='skill-window'>
-            <p className='text-black'>Préparer le déploiement d’une application sécurisée</p>
+        <div className='border-showskill-window'>
+          <div className='showskill-window'>
+
+            <div className='skill-window'>
+              <p className='top-mark-window text-black'>Développer une application sécurisée</p>
+            </div>
+            <div className='skill-window'>
+              <p className='top-mark-window text-black'>Concevoir et développer une application sécurisée organisée en couches</p>
+            </div>
+            <div className='skill-window'>
+              <p className='top-mark-window text-black'>Préparer le déploiement d’une application sécurisée</p>
+            </div>
+
           </div>
         </div>
         <div className='name-window'>
           <p className='text-white'>NOM</p>
         </div>
+
         <div className='more-window'>
           <p className='text-yellow'>VOIR +</p>
         </div>
