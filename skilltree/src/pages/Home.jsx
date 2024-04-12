3152a6b10fd911b53eac1dedb2bc4657c649312d
Ericdsr <eric62.dasilvarocha@laposte.net>
2024-04-12 12:05:14 +0200

    Add category 1

--- skilltree/src/pages/Home.jsx
+++ skilltree/src/pages/Home.jsx
@@ -6,10 +6,14 @@ function Home() {
   const [data, setData] = useState([]);
   const [selectedDivIndex, setSelectedDivIndex] = useState(null);
   const [selectedData, setSelectedData] = useState(null);
+  const [filteredDataCategory1, setFilteredDataCategory1] = useState([]);
+  const [filteredDataCategory2, setFilteredDataCategory2] = useState([]);
+  const [filteredDataCategory3, setFilteredDataCategory3] = useState([]);
+  const [firstCategory, setFirstCategory] = useState(null); // Ajout de l'état pour la première catégorie
+  const [category1Skills, setCategory1Skills] = useState(null); // Ajout de l'état pour les compétences de la catégorie 1
 
   useEffect(() => {
     fetchDataLearners();
-    fetchDataObtain();
   }, []);
 
   const fetchDataLearners = async () => {
@@ -25,39 +29,44 @@ function Home() {
       console.error("Error fetching data from 'Learners' table:", error.message);
     }
   };
+
   const fetchDataObtain = async (learnerId) => {
     try {
-      // Récupérer les données de la table "Obtain" pour un ID de Learner donné
       const { data: fetchedData, error } = await supabase
         .from('Obtain')
         .select('*')
         .eq('id_learner', learnerId); // Filtrer les données par ID de Learner
-  
+
       if (error) {
         throw error;
       }
-      // Récupérer les ID de compétence de la table "Obtain"
+
       const skillIds = fetchedData.map(obtainItem => obtainItem.id_skills);
-      // Récupérer les catégories correspondantes dans la table "Skills"
-      const categoriesResponse = await supabase
+
+      const category1SkillsResponse = await supabase
         .from('Skills')
-        .select('category') // Sélectionne uniquement la colonne "category"
-        .in('id', skillIds);
-      const categoriesData = categoriesResponse.data;
-      // Créer un tableau contenant uniquement les catégories
-      const categories = categoriesData.map(categoryItem => categoryItem.category);
-      // Afficher les catégories
-      console.log("Categories:", categories);
-      // Mettre à jour selectedData avec les données obtenues
-      setSelectedData(fetchedData);
-      console.log("Data from 'obtain' table:", fetchedData);
+        .select('id, rank')
+        .eq('category', '1');
+      const category1SkillsData = category1SkillsResponse.data;
+
+      const category1Skills = category1SkillsData.map(skillItem => ({
+        id: skillItem.id,
+        rank: skillItem.rank
+      }));
+
+      setCategory1Skills(category1Skills);
+
+      const filteredDataCategory1 = fetchedData.filter(item => 
+        category1Skills.some(skill => skill.id === item.id_skills)
+      );
+
+      setFilteredDataCategory1(filteredDataCategory1);
+      console.log("Filtered Data Category 1:", filteredDataCategory1);
     } catch (error) {
       console.error("Error fetching data from 'obtain' table:", error.message);
     }
   };
-  
 
-  // Tableau contenant les URLs de chaque musique correspondant à chaque div
   const musicUrls = [
     'Simon.mp3',
     'Cédric.mp3',
@@ -71,13 +80,12 @@ function Home() {
     'ThéoC.mp3',
     'William.mp3',
     'Vivien.mp3',
-    // Ajoutez autant d'URLs de musique que nécessaire
   ];
+
   const findIndexById = (id) => {
     return data.findIndex(item => item.id === id);
   };
-  
-  // Fonction pour activer ou désactiver la musique
+
   const toggleAudio = async (id) => {
     console.log("Toggle audio function called with ID:", id);
     const index = findIndexById(id);
@@ -87,11 +95,9 @@ function Home() {
       const learnerId = data[index].id;
       console.log("Learner ID:", learnerId);
   
-      // Vérifiez si la div cliquée est différente de celle déjà sélectionnée
       if (selectedDivIndex !== index) {
         console.log("New div clicked, updating selected data.");
         setSelectedDivIndex(index);
-        // Chargez les données de 'Obtain' uniquement si elles n'ont pas déjà été chargées
         await fetchDataObtain(learnerId);
       } else {
         console.log("Same div clicked again, resetting selected data and index.");
@@ -100,17 +106,13 @@ function Home() {
       }
     }
   };
-  
-  
-  
-  
+
   return (
     <div>
       <div className='dflexresize'>
         {data.map((Learner, index) => (
           <div key={Learner.id} onClick={() => toggleAudio(Learner.id)} className={'reglageimg ' + Learner.id}>           
             <img src={Learner.url} alt="" className='photomg' />
-            {/* Vérifier si cette div est actuellement sélectionnée */}
             {selectedDivIndex === index && (
               <audio controls autoPlay>
                 <source src={musicUrls[index]} type="audio/mp3" />
@@ -125,16 +127,16 @@ function Home() {
           <div className='showskill-window'>
             <div className='skill-window'>
               <p className='top-mark-window text-black'>Développer une application sécurisée</p>
-              <div>
-              <div>
-                  {selectedData && selectedData.map((Obtain) => (
-                    <div key={Obtain.id}>
-                      <p>ID Learner: {Obtain.id_learner}</p>
-                      <p>ID Skill: {Obtain.id_skills}</p>
-                    </div>
-                  ))}
+              {filteredDataCategory1.map((obtainItem) => (
+                <div key={obtainItem.id}>
+                  {/* Vérifier si category1Skills est défini avant de l'utiliser */}
+                  {category1Skills && (
+                    // Trouver le rang correspondant à l'id_skills dans les données des compétences de la catégorie 1
+                    category1Skills.find(skill => skill.id === obtainItem.id_skills)?.rank
+                  )}
                 </div>
-              </div>
+              ))}
+
             </div>
             <div className='skill-window'>
               <p className='top-mark-window text-black'>Concevoir et développer une application sécurisée organisée en couches</p>
@@ -142,13 +144,11 @@ function Home() {
             <div className='skill-window'>
               <p className='top-mark-window text-black'>Préparer le déploiement d’une application sécurisée</p>
             </div>
-
           </div>
         </div>
         <div className='name-window'>
           <p className='text-white'>NOM</p>
         </div>
-
         <div className='more-window'>
           <a className='text-yellow' href="/Details">VOIR +</a>
         </div>
